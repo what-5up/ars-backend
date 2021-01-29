4d4f0fa4fac2c1b4dec74b47d4ee53b6fd68c47f
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-01-30 02:50:15 +0530

    MOD - Scheduled flights can be filtered

--- models/scheduled-flight-model.js
+++ models/scheduled-flight-model.js
@@ -1,20 +1,52 @@
-const { pool }  = require(`../database/connection`);
+const { pool } = require(`../database/connection`);
 
 /**
  * Fetches all the bookings categorized by the passenger type from the database
  * 
+ * @param {string} origin default undefined
+ * @param {string} destination default undefined
+ * @param {string} aircraftID default undefined
+ * @param {string} aircraftModel default undefined
  * @returns {object} Promise of a query output
  * @throws Error
  */
-async function getScheduledFlights() {
+async function getScheduledFlights(origin = undefined, destination = undefined, aircraftID = undefined, aircraftModel = undefined) {
     return new Promise((resolve, reject) => {
-        const result = pool.query('SELECT * FROM scheduled_flight_fullinfo',
-            [],
+        //building the where clause
+        let whereClause = '';
+        let variableNames = [];
+        let variableValues = [];
+        if (origin !== undefined || destination !== undefined || aircraftID !== undefined || aircraftModel !== undefined) {
+            whereClause = ' WHERE '
+            if (origin !== undefined) {
+                variableNames.push('origin = ?');
+                variableValues.push(origin);
+            }
+            if (destination !== undefined) {
+                variableNames.push('destination = ?');
+                variableValues.push(destination);
+            }
+            if (aircraftID !== undefined) {
+                variableNames.push('aircraft_id = ?');
+                variableValues.push(aircraftID);
+            }
+            if (aircraftModel !== undefined) {
+                variableNames.push('aircraft_model = ?');
+                variableValues.push(aircraftModel);
+            }
+            (variableNames.length == 1) ? whereClause += variableNames[0] :
+                whereClause += variableNames.join(' AND ');
+        }
+
+        //fetching data from the database
+        const result = pool.query('SELECT * FROM scheduled_flights_list' + whereClause,
+            variableValues,
             function (error, results) {
                 if (error) {
                     reject(new Error(error.message));
                 }
                 resolve(results);
+                
             }
         );
     })
