2cf11f95c2238685c4425a38a69c3b7fd3c03e5e
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-01-29 23:38:31 +0530

    FIX - routes

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-require('dotenv').config()
-const express = require('express'),
-    http = require('http');
+const express = require('express');
+const app = express();
+const http = require('http');
 const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 5000;
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello world!</h1></body></html>');
-});
 
 require('./startup/routes')(app);
 
