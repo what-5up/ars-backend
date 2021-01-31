eaeff863ec2cf4e26943eeb21f8f0e9c0fcb9a0f
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-02-01 01:07:22 +0530

    ADD - View ticket price

--- models/price-model.js
+++ models/price-model.js
@@ -1,6 +1,47 @@
 const { pool } = require(`../database/connection`);
 
 /**
+ * Fetch all the ticket prices with their classes
+ * 
+ * @return {Promise<object>} query output
+ * @throws {Error} - database connection error
+ */
+async function fetchAllTicketPrices() {
+    return new Promise((resolve, reject) => {
+        pool.query("SELECT tc.class AS traveler_class, r.origin_code, r.origin, r.destination_code, r.destination, p.amount AS price FROM price p INNER JOIN traveler_class tc ON p.traveler_class = tc.id INNER JOIN route_with_airports r ON p.route_id = r.id",
+            [],
+            (error, results) => {
+                if (error) {
+                    reject(new Error(error.message));
+                }
+                resolve(results);
+            }
+        );
+    });
+}
+
+/**
+ * Fetch all the ticket prices with their classes
+ * 
+ * @param {string} route - route of the ticket prices to be fetched
+ * @return {Promise<object>} query output
+ * @throws {Error} - database connection error
+ */
+async function fetchTicketPrices(route) {
+    return new Promise((resolve, reject) => {
+        pool.query("SELECT tc.class AS traveler_class, p.amount AS price FROM price p INNER JOIN traveler_class tc ON p.traveler_class = tc.id INNER JOIN route_with_airports r ON p.route_id = r.id WHERE r.id = ?",
+            [route],
+            (error, results) => {
+                if (error) {
+                    reject(new Error(error.message));
+                }
+                resolve(results);
+            }
+        );
+    });
+}
+
+/**
  * Update Price for given route
  * 
  * @returns {object} Promise of a query output
@@ -42,6 +83,8 @@ const updateDiscount = async (id, discount) => {
 }
 
 module.exports = {
+    fetchAllTicketPrices,
+    fetchTicketPrices,
     updatePriceForRoute,
     updateDiscount
 }
