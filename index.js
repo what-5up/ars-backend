a023ef7adf009b57ac12f0004fe7df0efd46e28e
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-02-03 11:30:49 +0530

    enabled CORS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const bodyParser = require('body-parser');
-const logger = require('./utils/logger')
+const logger = require('./utils/logger');
 const middleware = require('./middlwares/middlewares');
 
 const hostname = 'localhost';
@@ -10,6 +10,13 @@ const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+});
 
 app.use(middleware.requestLogger);
 require('./startup/routes')(app);
