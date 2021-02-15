3fc1b2fcbcf8122167c2ea2f1d022ea1ee2f6739
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-02-15 16:31:46 +0530

    mod response format

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -81,23 +81,24 @@ const viewPassengerCountByDest = async (req,res) => {
     let endDate = req.query.endDate;
     const {error,value} = validatePassengerCountByDest(destination,startDate,endDate);
     if (error){
-        return res.status(422).json({message: error.details[0].message})
+        errorMessage(res, error.details[0].message, 422);
     }
     if ((value.startDate && value.endDate) && (value.startDate>value.endDate)) {
-        return res.status(422).json({message: "The start date must be before the end date"})
+        errorMessage(res, "The start date must be before the end date", 422);
     }
     startDate = (value.startDate===null)?undefined: value.startDate.toISOString().substring(0, 10);
     endDate = (value.endDate===null)?undefined: value.endDate.toISOString().substring(0, 10);
     try {
         const results = await model.getNoOfPassengersToDest(destination,startDate,endDate);
         if (results.length===0 || results[0].no_of_passengers===0){
-            return res.status(404).json({message: "Destination Passenger Count not found"});
+            errorMessage(res, "Destination Passenger Count not found", 404);
         }
-        res.status(200).json({result: results[0], message: "Destination Passenger Counts found"});
+        //res.status(200).json({result: results[0], message: "Destination Passenger Counts found"});
+        successMessage(res, results[0],'Destination Passenger Counts found',200);
     }
     catch (err) {
         console.log(err);
-        res.status(500).json({message:"Internal Server Error"});
+        errorMessage(res, "Internal Server Error", 500);
     }
 };
 
