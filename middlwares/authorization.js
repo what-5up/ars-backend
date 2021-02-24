02997412a811a38df2e27561bea13cb94995bfc2
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-02-25 01:49:30 +0530

    mod authorization middleware

--- middlwares/authorization.js
+++ middlwares/authorization.js
@@ -1,42 +1,18 @@
-const jwt = require('jsonwebtoken');
 const { successMessage, errorMessage } = require('../utils/message-template');
 
-//authentication
-
-const isAuth = (req,res,next)=>{
-    const authHeader = req.get('Authorization');      //token set to auth header by client
-    if (!authHeader){
-        return errorMessage(res,"Not authenticated.",401);
-    }
-    const token = authHeader.split(' ')[1]; //'Bearer token'-->['Bearer','token']-->'token'
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, 'somesupersecret');  //ENV
-    }
-    catch (err) {
-        errorMessage(res,"Internal Server Error",500);
-    }
-    if (!decodedToken){
-        return errorMessage(res,"Not authenticated.",401);
-    }
-    req.userID = decodedToken.userID;
-    req.accType = decodedToken.accType;
-    next();
-};
 
 //authorization
 
-const requiresAdmin = (req,res,next) =>{
-    if(req.accType !== 'a') {
-        return errorMessage(res,"Not authorized.",401);
-    }
-    else {
-        next();
-    }
-};
+function isAuthorized(accTypes=[]){    //all possible accTypes=['guest','normal user','frequent user','gold user','c','m','s','a']
+    return function(req, res, next) {
+        if (!accTypes.includes(req.accType)){
+            return errorMessage(res,"Not authorized.",401);
+        }
+        else {
+            next();
+        }
 
+    }
+}
 
-module.exports = {
-    isAuth,
-    requiresAdmin
-};
+module.exports = isAuthorized;
