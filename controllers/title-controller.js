4e79a23665b27f76e5b8fc2501fa1673d7987321
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-02-25 17:18:38 +0530

    add updateTitleById in controller

--- controllers/title-controller.js
+++ controllers/title-controller.js
@@ -1,5 +1,8 @@
-const titleModel = require("../models/title-model");
+const Joi = require('joi');
 const { successMessage, errorMessage } = require("../utils/message-template");
+
+const titleModel = require("../models/title-model");
+
 
 const getAllTitles = async (req, res) => {
     try {
@@ -11,6 +14,32 @@ const getAllTitles = async (req, res) => {
     }
 };
 
+function validateUpdateTitle(titleName) {
+    const schema = Joi.object({
+        titleName: Joi.string().trim().min(2).default(null).label('Title')
+    });
+    return schema.validate({titleName: titleName})
+}
+
+
+const updateTitle = async (req, res) => {
+    const titleId = req.params.titleId;
+    const titleName = req.body.titleName;
+    const { error, value } = validateUpdateTitle(titleName);
+    if (error) {
+        return errorMessage(res, error.details[0].message, 422)
+    }
+    try {
+        console.log(value.titleName);
+        const queryResult = await titleModel.updateTitleById(value.titleName,titleId)
+        return successMessage(res, null, 'Title updated successfully');
+    }
+    catch (err) {
+        errorMessage(res, "Internal server error", 500);
+    }
+};
+
 module.exports = {
-    getAllTitles
+    getAllTitles,
+    updateTitle
 };
