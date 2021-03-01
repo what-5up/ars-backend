ea4c0b932a0af3200eb7331c0a27faa9e4a8e36c
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-03-02 02:13:46 +0530

    MOD - validate function available for request queries

--- middlewares/schema-validators/validate.js
+++ middlewares/schema-validators/validate.js
@@ -8,13 +8,20 @@ const { errorMessage } = require('../../utils/message-template');
  * @param {Request} req - http request
  * @param {Response} res - http response
  * @param {Function} next - next middleware
+ * @param {boolean} isQuery - true - check request queries, false - check request body
  */
-const validate = (schema, req, res, next) => {
-    const { error, value } = schema.validate(req.body);
+const validate = (schema, req, res, next, isQuery = false) => {
+    
+    const input = isQuery ? req.query : req.body
+
+    const { error, value } = schema.validate(input);
 
     if (error) {
         return errorMessage(res, error.message, 422)
     }
+
+    if (isQuery) req.body = value;
+    else req.query = value;
 
     next();
 }
