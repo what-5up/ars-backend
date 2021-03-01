86074fc339202a4bf5639bb5712a29c47b3ea31b
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-03-01 14:08:59 +0530

    ADD - designation CRUD

---
+++ controllers/designation-controller.js
@@ -0,0 +1,76 @@
+const model = require("../models/designation-model");
+const { successMessage, errorMessage } = require("../utils/message-template");
+
+/**
+ * view all the designations of the system
+ * 
+ * @param {object} req - http request
+ * @param {object} res - http response
+ * @return {Response} [{ id, account_type_name, discount, criteria }]
+ */
+const viewAllDesignations = async (req, res, next) => {
+  model.getAllDesignations()
+    .then(result => {
+      successMessage(res, result);
+    })
+    .catch(err => next(err));
+}
+
+/**
+ * Add an designation
+ * 
+ * @param {object} req http request object
+ * @param {object} res http response object
+ * @return {Response} { id }
+ */
+const addDesignation = async (req, res, next) => {
+  model.addDesignation(req.body)
+    .then((result) => {
+      return successMessage(res, { id: result.insertId }, "designation added successfully");
+    })
+    .catch((error) => next(error));
+}
+
+/**
+ * Update a employee
+ * 
+ * @param {object} req http request object
+ * @param {object} res http response object
+ * @return {Response} promise of a record object
+ * @throws Error
+ */
+const updateDesignation = async (req, res, next) => {
+  model.updateDesignation(req.params.id, req.body)
+    .then(() => {
+      return successMessage(res, true, "Updated successfully");
+    })
+    .catch((error) => {
+      next(error)
+    });
+}
+
+
+/**
+ * Delete an account-type
+ * 
+ * @param {object} req http request object
+ * @param {object} res http response object
+ * @return {Response} { bool }
+ * @throws Error
+ */
+const deleteDesignation = async (req, res, next) => {
+  model
+    .deleteAccountType(req.params.id)
+    .then((result) => {
+      if (result == true) return successMessage(res, null, "designation deleted successfully")
+      else return errorMessage(res, "Unable to delete the accont type", 500);
+    })
+    .catch((error) => next(error));
+}
+
+module.exports = {
+  viewAllDesignations,
+  addDesignation,
+  updateDesignation,
+  deleteDesignation
+};
---
+++ models/designation-model.js
@@ -0,0 +1,126 @@
+const { pool } = require(`../database/connection`);
+
+/**
+ * get all the designations from the database
+ */
+async function getAllDesignations() {
+  return new Promise((resolve, reject) => {
+    const result = pool.query("SELECT * FROM designation",
+      function (error, results) {
+        if (error) {
+          reject(error);
+        }
+        resolve(results);
+      }
+    )
+  })
+}
+
+/**
+ * Add an new designation
+ *
+ * @param {object} payload containing attributes
+ * @returns {Promise<object>} Promise of a query output
+ * @throws Error
+ */
+const addDesignation = async (
+  payload = {
+    name: undefined,
+    privilege: undefined
+  }
+) => {
+  let fields = [];
+  let placeholders = [];
+  let values = [];
+  Object.keys(payload).forEach((key) => {
+    if (payload[key] != null) {
+      let updatedKey = key.split(/(?=[A-Z])/).join("_");
+      fields.push(`${updatedKey}`);
+      values.push(payload[key]);
+      placeholders.push("?")
+    }
+  });
+  return new Promise((resolve, reject) => {
+    pool.query(
+      `INSERT INTO designation (${fields.join()}) VALUES (${placeholders.join()})`,
+      values,
+      (error, result) => {
+        if (error) reject(error);
+        else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
+/**
+* Update an designation
+*
+* @param {int} id 
+* @param {object} payload parameters to change
+* @returns {Promise<object>} Promise of a query output
+* @throws Error
+*/
+const updateDesignation = async (
+  id,
+  payload = {
+    name: undefined,
+    privilege: undefined
+  }
+) => {
+  let fields = [];
+  let values = [];
+  Object.keys(payload).forEach((key) => {
+    if (payload[key] != null) {
+      let updatedKey = key.split(/(?=[A-Z])/).join("_");
+      fields.push(`${updatedKey} = ?`);
+      values.push(payload[key]);
+    }
+  });
+  return new Promise((resolve, reject) => {
+    pool.query(
+      `UPDATE designation
+        SET ${fields.join()}
+        WHERE id = ?`,
+      [...values, id],
+      (error, result) => {
+        if (error) reject(error);
+        if (result.affectedRows < 1) reject('Cannot find an designation for the given id');
+        resolve(result);
+      }
+    );
+  });
+};
+
+/**
+ * Delete an designation of the given an ID
+ *
+ * @param {string} id
+ * @returns {Promise<boolean>} Promise of a query output
+ * @throws Error
+ */
+const deleteDesignation = async (id) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE designation SET is_deleted = 1 WHERE id = ?",
+      [parseInt(id)],
+      (error, result) => {
+        if (error) reject(error);
+        else {
+          if (error) reject(error);
+          if (result.affectedRows < 1) reject('Cannot find an designation for the given id');
+          if (result.changedRows < 1) reject('designation of the given id is already deleted');
+          resolve(true);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  getAllDesignations,
+  addDesignation,
+  updateDesignation,
+  deleteDesignation
+};
